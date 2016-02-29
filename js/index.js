9d4a93cef5a1d508f6d665de074af8ca196fde47
Niels Masdorp <nielsmasdorp@gmail.com>
2016-02-29 15:04:25 +0100

    removed stuff

--- js/index.js
+++
@@ -1,7 +0,0 @@
-(function ($){
-    $(function () {
-        $('.button-collapse').sideNav();
-        $('.parallax').parallax();
-         $('.slider').slider({height: 800, full_width: true});
-    });
-})(jQuery);
