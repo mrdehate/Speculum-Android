ece969d6fb7c948128f522498eee44d848e1e4e9
Niels Masdorp <nielsmasdorp@gmail.com>
2016-02-29 14:53:52 +0100

    first version

---
+++ js/index.js
@@ -0,0 +1,7 @@
+(function ($){
+    $(function () {
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+         $('.slider').slider({height: 800, full_width: true});
+    });
+})(jQuery);
