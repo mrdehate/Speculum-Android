aa8c3d20d222f7663e59acffe0a82592d3e50d6f
Niels Masdorp <nielsmasdorp@gmail.com>
2016-02-29 12:54:44 +0100

    removed all code

--- app/src/main/java/com/nielsmasdorp/speculum/views/ISetupView.java
+++
@@ -1,9 +0,0 @@
-package com.nielsmasdorp.speculum.views;
-
-/**
- * @author Niels Masdorp (NielsMasdorp)
- */
-public interface ISetupView {
-
-    void navigateToMainActivity(String location, String subreddit, int pollingDelay, boolean celsius, boolean voiceCommands, boolean rememberConfig, boolean simpleLayout);
-}
