121bcecbca60b1121aab3a03575e038b72dfde92
Niels Masdorp <nielsmasdorp@gmail.com>
2016-03-01 23:44:55 +0100

    new website

---
+++ js/site.js
@@ -0,0 +1,14 @@
+(function ($){
+    $(function () {
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+        $('.slider').slider({full_width: true});
+
+        $(".nav-button").click(function() {
+    		$('html, body').animate({
+        		scrollTop: $('#' + this.id + "-container").offset().top - 50
+    		}, 2000);
+		});
+    });
+})(jQuery);
+
